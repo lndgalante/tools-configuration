5b5365cc85566ec80d2af2571fcad4da7bf413ba
Leonardo Galante <fenwil@fenwil.local>
2016-11-02 21:10:00 -0300

    homebrew, chrome, vscode, git, & request  updated

--- hyper-settings.js
+++ hyper-settings.js
@@ -2,37 +2,28 @@ module.exports = {
     config: {
         // default font size in pixels for all tabs
         fontSize: 15,
-
         // font family with optional fallbacks
-        fontFamily: '"Source Code Pro", monospace',
-
-        // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-        cursorColor: 'rgba(82,139,255,0.8)',
-
+        fontFamily: "Source Code Pro",
+        // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or
+        // cmyk)
+        cursorColor: 'rgba(82,139, 255, 0.8)',
         // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
         cursorShape: 'BLOCK',
-
         // color of the text
         foregroundColor: '#abb2be',
-
         // terminal background color
         backgroundColor: '#282c34',
-
         // border color (window, tabs)
         borderColor: '#333',
-
         // custom css to embed in the main window
         css: '',
-
         // custom css to embed in the terminal window
         termCSS: '',
-
         // custom padding (css format, i.e.: `top right bottom left`)
-        padding: '12px 14px',
-
-        // the full list. if you're going to provide the full color palette,
-        // including the 6 x 6 color cubes and the grayscale map, just provide
-        // an array here instead of a color map object
+        padding: '12px14px',
+        // the full list. if you're going to provide the full color palette, including
+        // the 6 x 6 color cubes and the grayscale map, just provide an array here
+        // instead of a color map object
         colors: {
             black: '#000000',
             red: '#ff0000',
@@ -51,40 +42,26 @@ module.exports = {
             lightCyan: '#00ffff',
             lightWhite: '#ffffff'
         },
-
-        // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-        // if left empty, your system's login shell will be used by default
+        // the shell to run when spawning a new session (i.e. /usr/local/bin/fish) if
+        // left empty, your system's login shell will be used by default
         shell: '',
-
-        // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
-        // by default ['--login'] will be used
+        // for setting shell arguments (i.e. for using interactive shellArgs: ['-i']) by
+        // default ['--login'] will be used
         shellArgs: ['--login'],
-
         // for environment variables
         env: {},
-
         // set to false for no bell
         bell: 'SOUND',
-
         // if true, selected text will automatically be copied to the clipboard
         copyOnSelect: false
-
-        // URL to custom bell
-        // bellSoundURL: 'http://example.com/bell.mp3',
-
-        // for advanced config flags please refer to https://hyper.is/#cfg
+        // URL to custom bell bellSoundURL: 'http://example.com/bell.mp3', for advanced
+        // config flags please refer to https://hyper.is/#cfg
     },
-
-    // a list of plugins to fetch and install from npm
-    // format: [@org/]project[#version]
-    // examples:
-    //   `hyperpower`
-    //   `@company/project`
-    //   `project#1.0.1`
+    // a list of plugins to fetch and install from npm format:
+    // [@org/]project[#version] examples:   `hyperpower`   `@company/project`
+    // `project#1.0.1`
     plugins: [],
-
-    // in development, you can create a directory under
-    // `~/.hyper_plugins/local/` and include it here
-    // to load it and avoid it being `npm install`ed
+    // in development, you can create a directory under `~/.hyper_plugins/local/`
+    // and include it here to load it and avoid it being `npm install`ed
     localPlugins: []
 };
