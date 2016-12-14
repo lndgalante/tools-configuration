1bfab5c7146175b752c8f8f1ca1e775c34e39f0a
Fenwil <lndgalante@gmail.com>
2016-12-14 11:28:52 -0300

    beautify removed

--- hyper-settings.js
+++ hyper-settings.js
@@ -91,7 +91,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [`hyper-simple-vibrancy`],
+  plugins: [],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
