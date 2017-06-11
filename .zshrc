ff1fa5798189b549d02677b0886d2ff9f7e5bb43
Fenwil <lndgalante@gmail.com>
2017-06-11 14:04:00 -0300

    Update setup

--- .zshrc
+++ .zshrc
@@ -14,6 +14,7 @@ clear
 neofetch
 
 alias adb-shake="adb shell input keyevent 82"
+alias git-undo="git reset --soft HEAD~1"
 
 export ANDROID_HOME=~/Library/Android/sdk
 export GOPATH=~/go
@@ -21,3 +22,9 @@ export PATH=${PATH}:${GOPATH}/bin
 export PATH=${PATH}:${ANDROID_HOME}/tools
 export PATH=${PATH}:${ANDROID_HOME}/platform-tools
 export PATH="$HOME/.fastlane/bin:$PATH"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+eval "$(thefuck --alias)"
--- setup.sh
+++ setup.sh
@@ -39,20 +39,11 @@ sudo defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 printf "Finder - Disable the warning when changing a file extension\n"
 sudo defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-printf "Finder - Show path bar\n"
-sudo defaults write com.apple.finder ShowPathbar -bool true
-
-printf "Finder - Show status bar\n"
-sudo defaults write com.apple.finder ShowStatusBar -bool true
-
 printf "Finder - Display full POSIX path as window title\n"
 sudo defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
 printf "Finder - Use list view in all Finder windows\n"
 sudo defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-
-printf "Finder - Allow quitting via COMMAND+Q -- Doing so will also hide desktop icons\n"
-sudo defaults write com.apple.finder QuitMenuItem -bool true
 
 printf "Finder - Disable the warning before emptying the Trash\n"
 sudo defaults write com.apple.finder WarnOnEmptyTrash -bool false
@@ -79,12 +70,6 @@ sudo defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 printf "Printer - Automatically quit printer app once the print jobs complete\n"
 sudo defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
-printf "App Store - Enable the WebKit Developer Tools in the Mac App Store\n"
-sudo defaults write com.apple.appstore WebKitDeveloperExtras -bool true
-
-echo "Disabling local Time Machine backups"
-sudo hash tmutil &> /dev/null && sudo tmutil disablelocal
-
 echo "Check for software updates daily, not just once per week"
 sudo defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
@@ -101,110 +86,42 @@ echo "Sort Activity Monitor results by CPU usage"
 sudo defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
 sudo defaults write com.apple.ActivityMonitor SortDirection -int 0
 
+echo "Install Homebrew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 echo "Homebrew installs"
-brew install bash-completion2
-brew install brew-cask
-brew install ack
-brew install wget
-brew install bash
-brew install vim
-brew install ccat
-brew install shellcheck
-brew install tree
 brew install git
-brew install the_silver_searcher
-brew install node
-brew install npm
-brew install python3
-brew install bpython
-brew install curl
-brew install libevent
+brew install yarn
 brew install openssl
 brew install nvm
 brew install thefuck
-brew install findutils
-brew install rename
-brew install readline
 brew install git-extras
-brew install coreutils
-brew install ant
-brew install autoconf
-brew install automake
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
 brew install pandoc
+brew install brew-cask
 brew install brew-pip
 brew install brew-gem
 brew install zsh
-brew install fish
-brew install ctunnel
-brew install dnscryptproxy
-brew install privoxy
-brew install dnsmasq
-brew install sshuttle
-brew install zshlovers
 brew install zsh-syntax-highlighting
-brew install ncrack
+brew install wine
+brew install playonmac
 brew install wifi-password
-brew install xcv
-brew install autojump
+brew install fcrackzip
+brew install aircrack-ng
+brew install ncrack
 brew cleanup
 
 echo "Brew cask installs"
+brew cask install hyper
 brew cask install google-chrome
-brew cask install lastpass
-brew cask install firefox
+brew cask install visual-studio-code
 brew cask install vlc
+brew cask install webtorrent
+brew cask install slack
+brew cask install ccleaner
+brew cask install mounty
 brew cask install the-unarchiver
-brew cask install iterm2
-brew cask install teamviewer
-brew cask install google-hangouts
-brew cask install flickr-uploadr
-brew cask install namechanger
-brew cask install caffeine
-brew cask install gas-mask
-brew cask install transmission
+brew cask install recordit
 brew cask install virtualbox
-brew cask install appcleane
-brew cask install 1clipboard
-brew cask install cakebrew
-brew cask install coteditor
-brew cask install abricotine
-brew cask install proxpn
-brew cask install macpass
-brew cask install keka
-brew cask install megasync
-brew cask install dnscrypt
-brew cask install apppolice
-brew cask install clipy
-brew cask install betterzipql
-brew cask install qlcolorcode
-brew cask install qlmarkdown
-brew cask install qlprettypatch
-brew cask install qlstephen
 brew cask install quicklook-csv
 brew cask install quicklook-json
-brew cask install suspicious-package
-brew cask install webp-quicklook
-brew cask install menucalendarclock-ical
-brew cask install vmware-fusion
 brew cask cleanup
