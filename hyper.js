854393ef1aafedcb67aa31b159b244f21ec68688
Leonardo Galante <lndgalante@gmail.com>
2017-07-27 09:00:51 -0300

    Add hyper-transparent

--- hyper.js
+++ hyper.js
@@ -104,7 +104,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyper-snazzy', 'hyper-statusline'],
+  plugins: ['hyper-snazzy', 'hyper-statusline', 'hyper-transparent'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
