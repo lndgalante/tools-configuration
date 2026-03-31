365cfa9ebda0fea52d8cc39882598cba33533db6
Leonardo Galante <lndgalante@gmail.com>
2026-03-31 17:58:03 -0300

    Change alias 'claude' effort level to high

--- config.fish
+++ config.fish
@@ -9,7 +9,8 @@ cd $HOME/Developer/
 ls
 
 # alias claude="command claude --enable-auto-mode --model claude-opus-4-6 --effort max"
-alias claude="command claude --dangerously-skip-permissions --model claude-opus-4-6 --effort max"
+alias claude="command claude --dangerously-skip-permissions --model claude-opus-4-6 --effort high
+"
 
 
 
